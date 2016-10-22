cb5ba77a86722ff8f26e9fa5de9cf506d8ab01ff
musyoku <musyoku@users.noreply.github.com>
2016-10-22 18:28:43 +0900

    j

---
+++ __init__.py
@@ -0,0 +1,14 @@
+import link
+import function
+import util
+from sequential import *
+
+def from_json(str):
+	seq = Sequential()
+	seq.from_json(str)
+	return seq
+
+def from_dict(dict_array):
+	seq = Sequential()
+	seq.from_dict(dict_array)
+	return seq
--- sequential.py
+++ sequential.py
@@ -3,32 +3,10 @@ import chainer
 import link
 import function
 
-class Chain(chainer.Chain):
-
-	def __init__(self, layers):
-		super(Chain, self).__init__()
-		self.n_layers = len(layers)
-		for name, layer in layers.iteritems():
-			if isinstance(layer, chainer.link.Link):
-				self.add_link(name, layer)
-			else:
-				setattr(self, name, layer)
-
-	def __call__(self, x, test=False):
-		for i in xrange(self.n_layers):
-			layer = getattr(self, "layer_%d" % i)
-			if isinstance(layer, function.dropout):
-				x = layer(x, train=not test)
-			elif isinstance(layer, chainer.links.BatchNormalization):
-				x = layer(x, test=test)
-			else:
-				x = layer(x)
-		return x
-
 class Sequential(object):
 	def __init__(self, weight_initializer="Normal", weight_init_std=1):
 		self._layers = []
-		self.chain = None
+		self.links = []
 
 		self.weight_initializer = weight_initializer	# Normal / GlorotNormal / HeNormal
 		self.weight_init_std = weight_init_std
@@ -99,29 +77,45 @@ class Sequential(object):
 		json = self.to_json()
 		self.from_json(json)
 
-	def to_json(self):
+	def to_dict(self):
 		result = []
 		for layer in self._layers:
 			config = layer.to_dict()
-			dict = {}
+			dic = {}
 			for key, value in config.iteritems():
 				if isinstance(value, (int, float, str, bool, type(None), tuple, list, dict)):
-					dict[key] = value
-			result.append(dict)
+					dic[key] = value
+			result.append(dic)
+		return result
 
+	def to_json(self):
+		result = self.to_dict()
 		return json.dumps(result, sort_keys=True, indent=4, separators=(',', ': '))
 
 	def from_json(self, str):
-		self.chain = None
+		self.links = []
 		self._layers = []
 		attributes = {}
 		dict_array = json.loads(str)
+		self.from_dict(dict_array)
+
+	def from_dict(self, dict_array):
 		for i, dict in enumerate(dict_array):
 			layer = self.layer_from_dict(dict)
 			link = self.layer_to_chainer_link(layer)
-			attributes["layer_%d" % i] = link
+			self.links.append(link)
 			self._layers.append(layer)
-		self.chain = Chain(attributes)
+
 
 	def __call__(self, x, test=False):
-		return self.chain(x, test)
+		return self.links(x, test)
+
+	def __call__(self, x, test=False):
+		for i, link in enumerate(self.links):
+			if isinstance(link, function.dropout):
+				x = link(x, train=not test)
+			elif isinstance(link, chainer.links.BatchNormalization):
+				x = link(x, test=test)
+			else:
+				x = link(x)
+		return x
