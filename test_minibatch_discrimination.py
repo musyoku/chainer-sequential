ac78ad9f543a33fe11e0af0357508ecfb2293a5c
musyoku <musyoku@users.noreply.github.com>
2016-10-30 13:00:52 +0900

    j

--- test_minibatch_discrimination.py
+++
@@ -1,7 +0,0 @@
-from chainer import Variable
-import numpy as np
-from minibatch_discrimination import MinibatchDiscrimination
-
-x = Variable(np.random.normal(size=(4, 10)).astype(np.float32))
-layer = MinibatchDiscrimination(10, 5, 30)
-print layer(x).data
