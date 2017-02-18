523d0ad5183deecaa9d0647fc60b1e50dd6e3751
musyoku <musyoku@users.noreply.github.com>
2017-02-19 01:07:35 +0900

    :v:

--- util.py
+++ util.py
@@ -63,29 +63,29 @@ def get_deconv_insize(out_size, ksize, stride, padding, cover_all=False):
 	else:
 		return (out_size - ksize + 2 * padding) // stride + 1
 
-def get_paddings_of_deconv_layers(target_size, num_layers, ksize, stride):
+def get_paddings_of_deconv_layers(out_size, num_layers, ksize, stride):
 	# compute required deconv paddings
 	paddings = []
-	deconv_out_sizes = [target_size]
+	deconv_out_sizes = [out_size]
 	for i in xrange(num_layers):
 		deconv_out_sizes.append(get_conv_outsize(deconv_out_sizes[-1], ksize, stride, get_conv_padding(deconv_out_sizes[-1], ksize, stride)))
 		
-	# target_size of hidden layer must be an even number
+	# out_size of hidden layer must be an even number
 	for i, size in enumerate(deconv_out_sizes[1:-1]):
 		if size % 2 == 1:
 			deconv_out_sizes[i+1] = size + 1
 
 	deconv_out_sizes.reverse()
 	paddings = []
-	for i, (in_size, target_size) in enumerate(zip(deconv_out_sizes[:-1], deconv_out_sizes[1:])):
-		paddings.append(get_deconv_padding(in_size, target_size, ksize, stride))
+	for i, (in_size, out_size) in enumerate(zip(deconv_out_sizes[:-1], deconv_out_sizes[1:])):
+		paddings.append(get_deconv_padding(in_size, out_size, ksize, stride))
 
 	return paddings
 
-def get_in_size_of_deconv_layers(target_size, num_layers, ksize, stride):
+def get_in_size_of_deconv_layers(out_size, num_layers, ksize, stride):
 	# compute required deconv paddings
 	paddings = []
-	deconv_out_sizes = [target_size]
+	deconv_out_sizes = [out_size]
 	for i in xrange(num_layers):
 		deconv_out_sizes.append(get_conv_outsize(deconv_out_sizes[-1], ksize, stride, get_conv_padding(deconv_out_sizes[-1], ksize, stride)))
 	return deconv_out_sizes[-1]
